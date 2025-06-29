b338c2a8e49b90709e7143941002cc7a55c38260
Khushal Mali <khushalmali8448@gmail.com>
2025-06-30 02:28:30 +0530

    update metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+  title = "Layervibe - Transform images without losing details.",
+  description = "Upload a photo, describe what you want — our AI does the rest. We edit images and transform them into videos without lost the importante details.",
+  image = "/logo.svg",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      // creator: "@khushalmali_x",
+    },
+    icons,
+  };
 }
