f04a9648fc6e449c03febcd11902245c059b9f5b
Khushal Mali <khushalmali8448@gmail.com>
2025-07-04 16:09:30 +0530

    update features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,9 @@ const Home = () => {
             More than images, time optimization.
           </p>
 
-          <div className="mx-auto flex h-[589px] w-full gap-4 bg-amber-400">
-            <div className="h-full max-w-[416px] flex-1 overflow-hidden rounded-md bg-sky-300">
-              <div className="relative aspect-[416/449] bg-emerald-400">
+          <div className="mx-auto flex h-[589px] w-full gap-4">
+            <div className="h-full max-w-[416px] flex-1 overflow-hidden rounded-md bg-white shadow-sm">
+              <div className="relative aspect-[416/449]">
                 <Image
                   fill
                   src={"/features-section-1-image.svg"}
@@ -45,8 +45,8 @@ const Home = () => {
                 </p>
               </div>
             </div>
-            <div className="flex h-full flex-1 flex-col gap-4 bg-pink-400">
-              <div className="flex h-full w-full overflow-hidden rounded-md bg-stone-400">
+            <div className="flex h-full flex-1 flex-col gap-4">
+              <div className="flex h-full w-full overflow-hidden rounded-md shadow-sm">
                 <div className="flex h-full w-1/2 items-center justify-center bg-[#1A1C1E]">
                   <div className="max-w-[343px] space-x-2">
                     <h5 className="font-inter text-[20px] leading-[150%] font-semibold text-white">
@@ -77,46 +77,52 @@ const Home = () => {
                   <div className="absolute inset-0 z-20 bg-gradient-to-r from-[#1A1C1E] to-[#1A1C1E00]"></div>
                 </div>
               </div>
+              <div className="h-full w-full rounded-md bg-white shadow-sm">
+                <div className="mx-auto flex h-full max-w-[550px] flex-col gap-8 py-4">
+                  <div className="flex flex-col gap-2">
+                    <h3 className="font-inter text-center text-[20px] leading-[150%] font-semibold text-[#131416]">
+                      Test multiple variations with masks.
+                    </h3>
+                    <p className="font-inter text-center text-sm leading-[130%] font-normal text-[#363B3F]">
+                      Generate variations of the same image at once by creating
+                      different masks. Create the mask you want, write your
+                      prompt, and you’re done.
+                    </p>
+                  </div>
 
-              <div className="mx-auto flex h-full max-w-[550px] flex-col gap-8 rounded-md bg-cyan-400 py-4">
-                <div className="flex flex-col gap-2">
-                  <h3 className="font-inter text-center text-[20px] leading-[150%] font-semibold text-[#131416]">
-                    Test multiple variations with masks.
-                  </h3>
-                  <p className="font-inter text-center text-sm leading-[130%] font-normal text-[#363B3F]">
-                    Generate variations of the same image at once by creating
-                    different masks. Create the mask you want, write your
-                    prompt, and you’re done.
-                  </p>
-                </div>
-
-                <div className="mx-auto flex gap-3">
-                  {imageVariation.map(({ imageUrl, text }, i) => (
-                    <div
-                      key={i}
-                      className="flex h-[136px] w-[88px] flex-col gap-2.5 bg-indigo-400"
-                    >
-                      <div className="relative aspect-[88/108] overflow-hidden rounded-[10px] bg-[#ECB006]">
-                        <Image
-                          fill
-                          src={imageUrl}
-                          alt={text}
-                          className="object-cover"
-                        />
-                      </div>
-
+                  <div className="mx-auto flex gap-3">
+                    {imageVariation.map(({ imageUrl, text }, i) => (
                       <div
-                        className={cn(
-                          "font-inter mx-auto h-[19px] rounded-xl border-[1px] px-2 py-[2px] text-center text-[10px] leading-[150%] font-medium tracking-wide",
-                          text === "Original"
-                            ? "border-[#EDFF50] bg-[#F8FF90] text-[#708B00]"
-                            : "border-[#DEE1E3] bg-[#E6E8EA] text-[#363B3F]",
-                        )}
+                        key={i}
+                        className="flex h-[136px] w-[88px] flex-col gap-2.5"
                       >
-                        {text}
+                        <div
+                          className={cn(
+                            "relative aspect-[88/108] overflow-hidden rounded-[10px]",
+                            i === 2 && "bg-[#ECB006]",
+                          )}
+                        >
+                          <Image
+                            fill
+                            src={imageUrl}
+                            alt={text}
+                            className="object-cover"
+                          />
+                        </div>
+
+                        <div
+                          className={cn(
+                            "font-inter mx-auto h-[19px] rounded-xl border-[1px] px-2 py-[2px] text-center text-[10px] leading-[150%] font-medium tracking-wide",
+                            text === "Original"
+                              ? "border-[#EDFF50] bg-[#F8FF90] text-[#708B00]"
+                              : "border-[#DEE1E3] bg-[#E6E8EA] text-[#363B3F]",
+                          )}
+                        >
+                          {text}
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
